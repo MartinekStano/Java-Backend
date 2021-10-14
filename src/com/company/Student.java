bd1b3bf21be3f176269505eed5c01b72507d6057
Martinekk <martinmatostano@gmail.com>
2021-10-14 08:29:06 +0200

    add new class "Student" and create new object "student"

---
+++ src/com/company/Student.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Student {
+    public String Name;
+    public int NumberOfStudent;
+
+    public Student(String name, int numberOfStudent) {
+        Name = name;
+        NumberOfStudent = numberOfStudent;
+    }
+
+}
