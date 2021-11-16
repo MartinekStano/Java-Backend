1ff937a0203e43d8f4cae3781179bb2aeb03b0e8
Martinekk <martinmatostano@gmail.com>
2021-11-16 10:04:45 +0100

    add method of add new item if it doesn't exist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,63 @@
 package com.company;
 
+import java.awt.font.FontRenderContext;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        HashMap<Integer, String> hashMap = new HashMap<>();
 
-        hashMap.put("Red", 1);
-        hashMap.put("Green", 2);
-        hashMap.put("Black", 3);
-        hashMap.put("White", 4);
+        hashMap.put(1,"Red");
+        hashMap.put(2,"Green");
+        hashMap.put(3,"Black");
+        hashMap.put(4,"White");
 
+        Integer key1 = 0;
+        Integer key2 = 2;
+
+        String value1 = "Grey";
+        String value2 = "Red";
+
+        addNewItemIfDoesntExit(hashMap, value1, key1);
+        showHashMap(hashMap);
+
+        System.out.println();
+
+        addNewItemIfDoesntExit(hashMap, value2, key2);
+        showHashMap(hashMap);
+    }
+    public static void addNewItemIfDoesntExit(HashMap<Integer, String> hashMap, String value, Integer key)
+    {
+        if(!hashMap.containsKey(key))
+        {
+            if(!hashMap.containsValue(value))
+            {
+                System.out.println("Item added in HashMap");
+                hashMap.put(key, value);
+            }else{
+                System.out.println("Item already exist");
+            }
+        }else{
+            System.out.println("Item already exist");
+        }
+    }
+    public static void showHashMap(HashMap<Integer, String > hashMap)
+    {
         System.out.println("Keys: ");
-        for(String i : hashMap.keySet() )
+        for(Integer i : hashMap.keySet() )
         {
             System.out.print(i+", ");
         }
         System.out.println();
         System.out.println("Values: ");
-        for (Integer i : hashMap.values())
+        for (String i : hashMap.values())
         {
             System.out.print(i+", ");
         }
+        System.out.println();
 
     }
+
 }
