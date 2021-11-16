abe9223e57dcf8488ca4c27ecb25e403d9aff6d2
Martinekk <martinmatostano@gmail.com>
2021-11-16 09:19:22 +0100

    add HashSet instead of List

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,29 +2,20 @@ package com.company;
 
 import java.util.*;
 
-public class Main extends Shiritori {
+public class Main {
 
     public static void main(String[] args) {
 
-        ContainsCharInList containsCharInList = (List<Person> personList) ->{
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(1);
+        hashSet.add(1);
+        hashSet.add(1);
+        hashSet.add(2);
+        hashSet.add(2);
+        hashSet.add(3);
+        hashSet.add(4);
+        hashSet.add(5);
 
-        boolean result = personList.stream()
-                            .anyMatch(e -> e.getName().contains("a"));
-            System.out.println(result);
-        };
-        Person person1 = new Person("John", 21, 23000 );
-        Person person2 = new Person("Steve", 32, 40000);
-        Person person3 = new Person("Martin", 16, 2700);
-
-        List<Person> personList = new ArrayList<>();
-
-        personList.add(person1);
-        personList.add(person2);
-        personList.add(person3);
-
-        containsCharInList.containsChar(personList);
-    }
-    interface ContainsCharInList{
-        void containsChar(List<Person> personList);
+        hashSet.forEach(e -> System.out.println(e));
     }
 }
