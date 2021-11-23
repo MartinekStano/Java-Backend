6ad8f7c90f030ca50aea32356de14b05197a84ee
Martinekk <martinmatostano@gmail.com>
2021-11-23 09:02:33 +0100

    rewrite getInfo method in programmer class

--- src/com/company/Programmer.java
+++ src/com/company/Programmer.java
@@ -5,4 +5,7 @@ public class Programmer extends Employee {
     public Programmer(float salary, int bonus) {
         super(salary, bonus, EnumEmployee.PROGRAMMER);
     }
+    public void getInfo(){
+        System.out.println("Programmer's salary is "+(salary+bonus));
+    }
 }
